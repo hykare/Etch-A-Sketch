a5e3078db4982f9b2d488c9fd226da0a78c4759a
aleksandra <78385964+hykare@users.noreply.github.com>
2021-08-20 17:13:30 +0200

    make pixels turn black when hovered over

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 const sketch = document.getElementById("sketch");
 
-let gridSize = 16;
+let gridSize = 32;
 sketch.style.gridTemplateColumns= `repeat(${gridSize}, 1fr)`;
-sketch.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
-for (let i = 0; i < gridSize*16; i++) {
+// sketch.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
+for (let i = 0; i < gridSize*gridSize; i++) {
     const square = document.createElement('div');
     square.classList.add('grid-square');
     square.style.width = '100%';
     square.style.height = '100%';
     sketch.appendChild(square);
+}
+
+sketch.addEventListener('mouseover', colorPixel);
+
+function colorPixel(e){
+
+    e.target.style.backgroundColor = 'black';
 }
