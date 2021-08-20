1d8ede90b4186c89bc3d64b86d4826b81376c10f
aleksandra <78385964+hykare@users.noreply.github.com>
2021-08-20 20:44:22 +0200

    buttons switch between black and random rainbow colors

--- script.js
+++ script.js
@@ -1,30 +1,58 @@
 const sketch = document.getElementById("sketch");
 let gridSize = 32;
-
 setUpGrid();
+
+
+
+const blackButton = document.getElementById('black');
+const rainbowButton = document.getElementById('rainbow');
+const clearButton = document.getElementById('clear');
+blackButton.addEventListener('click', setColorBlack);
+rainbowButton.addEventListener('click', setColorRainbow);
 
 
 
 sketch.addEventListener('mouseover', fillSquare);
 
-function fillSquare(e){
+function fillSquare(e) {
     currentOpacity = e.target.style.opacity;
-    if(currentOpacity===''){
+    if (currentOpacity === '') {
         e.target.style.opacity = '0.2';
     }
-    else e.target.style.opacity = `${+currentOpacity+0.2}`;
+    else e.target.style.opacity = `${+currentOpacity + 0.2}`;
 }
 
-function setUpGrid(){
-    sketch.style.gridTemplateColumns= `repeat(${gridSize}, 1fr)`;
+function setUpGrid() {
+    sketch.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
     createGridSquares();
 }
-function createGridSquares(){
-    for (let i = 0; i < gridSize*gridSize; i++) {
+function createGridSquares() {
+    for (let i = 0; i < gridSize * gridSize; i++) {
         const square = document.createElement('div');
         square.classList.add('grid-square');
         square.style.width = '100%';
         square.style.height = '100%';
         sketch.appendChild(square);
     }
+}
+function setColorBlack() {
+    const squares = sketch.childNodes;
+    squares.forEach(square => {
+        square.style.backgroundColor = 'black';
+    });
+
+}
+function setColorRainbow() {
+    const squares = sketch.childNodes;
+    squares.forEach(square => {
+
+        const hue = (Math.random()*360).toFixed(2);
+        console.log(hue)
+        square.style.backgroundColor = `hsl(${hue}, 100%, 50%)`;
+
+
+    });
+
+
+
 }
