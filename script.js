c4b0d51d85cfc1ba88174b62a3d2baece744ca3f
aleksandra <78385964+hykare@users.noreply.github.com>
2021-08-20 20:10:14 +0200

    make pixels turn darker gradually

--- script.js
+++ script.js
@@ -1,19 +1,30 @@
 const sketch = document.getElementById("sketch");
-
 let gridSize = 32;
-sketch.style.gridTemplateColumns= `repeat(${gridSize}, 1fr)`;
-// sketch.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
-for (let i = 0; i < gridSize*gridSize; i++) {
-    const square = document.createElement('div');
-    square.classList.add('grid-square');
-    square.style.width = '100%';
-    square.style.height = '100%';
-    sketch.appendChild(square);
+
+setUpGrid();
+
+
+
+sketch.addEventListener('mouseover', fillSquare);
+
+function fillSquare(e){
+    currentOpacity = e.target.style.opacity;
+    if(currentOpacity===''){
+        e.target.style.opacity = '0.2';
+    }
+    else e.target.style.opacity = `${+currentOpacity+0.2}`;
 }
 
-sketch.addEventListener('mouseover', colorPixel);
-
-function colorPixel(e){
-
-    e.target.style.backgroundColor = 'black';
+function setUpGrid(){
+    sketch.style.gridTemplateColumns= `repeat(${gridSize}, 1fr)`;
+    createGridSquares();
+}
+function createGridSquares(){
+    for (let i = 0; i < gridSize*gridSize; i++) {
+        const square = document.createElement('div');
+        square.classList.add('grid-square');
+        square.style.width = '100%';
+        square.style.height = '100%';
+        sketch.appendChild(square);
+    }
 }
